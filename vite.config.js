d317bbf5a4d84b2efa1ab625ed5fc19e07a41395
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-06-18 23:41:10 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,9 @@ export default defineConfig({
   server: {
     port: 7777,
   },
+  ssr: {
+    external: ["react-dom/server"]
+  },
   plugins: [
     mdx({
       rehypePlugins: [[rehypeImgSize, { dir: 'public' }], rehypeSlug, rehypePrism],
