bb3c243dc5b9d65c5c48e8f01617992b140963d9
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-06-19 15:23:09 +0530

    updated title

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -47,8 +47,8 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'Designer + Developer',
-    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+    title: 'Developer',
+    description: `Developer portfolio of ${config.name}`,
   });
 };
 
