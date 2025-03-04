31c21db91cd6a1923d83e1cb8552ac6bace470db
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2025-03-04 11:01:02 +0530

    Added Resume

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -7,7 +7,7 @@ import styles from './footer.module.css';
 export const Footer = ({ className }) => (
   <footer className={classes(styles.footer, className)}>
     <Text size="s" align="center">
-      <Link secondary className={styles.link} href="/humans.txt" target="_self">
+      <Link secondary className={styles.link} href="https://drive.google.com/file/d/1xotLKNvopW52k0qUVHqCufX3-uQJtYbL/view?usp=sharing" target="_self">
         Himanshu Thakur
       </Link>
     </Text>
